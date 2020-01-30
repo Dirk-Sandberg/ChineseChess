2e1472e1987720a672a742553278bd51ae45c7bb
eriksandberg <eriksandbergum@gmail.com>
2020-01-30 16:34:52 -0600

    Pieces are now "captured" by enemies and shown in a grid on the capturer's side of the board.

--- chesspiece.py
+++ chesspiece.py
@@ -14,20 +14,27 @@ class ChessPiece(ButtonBehavior, Image):
         app = App.get_running_app()
         board1 = app.root.ids.game_screen.ids.top_board
         board2 = app.root.ids.game_screen.ids.bottom_board
+        black_captured_pieces_grid = app.root.ids.game_screen.ids.captured_black_pieces
+        red_captured_pieces_grid = app.root.ids.game_screen.ids.captured_red_pieces
 
-        # If they clicked on a blank spot, move their piece
-        if self.piece_type == "blank":
-            if self.indicator_opacity == 1:
-                self.piece_type = app.highlighted_piece.piece_type
-                self.player = app.highlighted_piece.player
-                app.highlighted_piece.piece_type = 'blank'
+        # If they clicked on a highlighted spot, move their piece
+        if self.indicator_opacity == 1:
+            if self.piece_type != 'blank':
+                if self.player == 'black':
+                    red_captured_pieces_grid.add_widget(Image(source=self.source))
+                else:
+                    black_captured_pieces_grid.add_widget(Image(source=self.source))
+
+            # The piece has been captured if it wasn't blank!
+            self.piece_type = app.highlighted_piece.piece_type
+            self.player = app.highlighted_piece.player
+            app.highlighted_piece.piece_type = 'blank'
             # Clear all indicators
             for child in board1.walk():
                 child.indicator_opacity = 0
             for child in board2.walk():
                 child.indicator_opacity = 0
             return
-
         else:
             app.highlighted_piece = self
         # Clear all indicators
