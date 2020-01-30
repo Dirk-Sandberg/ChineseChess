95ac067c79b2a9f1a7444c41a95dcc3926ee3f98
eriksandberg <eriksandbergum@gmail.com>
2020-01-30 13:11:22 -0600

    Starting positions for each piece on the board has been set

--- chesspiece.py
+++ chesspiece.py
@@ -1,17 +1,23 @@
 from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
 from kivy.app import App
+from kivy.properties import OptionProperty, StringProperty
+
 
 class ChessPiece(ButtonBehavior, Image):
+    piece_type = OptionProperty("pawn", options=["rook", "cannon","pawn", "king", "knight", "guard", "elephant"])
+    player = StringProperty("black")
     def highlight_moves(self):
         app = App.get_running_app()
         board1 = app.root.ids.game_screen.ids.top_board
-        board2 = app.root.ids.game_screen.ids.top_board
+        board2 = app.root.ids.game_screen.ids.bottom_board
         for child in board1.walk():
             if isinstance(child, Image):
                 if child.source == 'dot.png':
                     child.parent.remove_widget(child)
-        im1 = board1.get_widget_at(1,3)
-        im = Image(source="dot.png", size=(im1.size[0]/2., im1.size[1]/2.), center=im1.center)
-        im1.add_widget(im)
+        im1 = board1.get_widget_at(4,8)
+        im1.color = [0,1,0,1]
+        print(im1.player, im1.piece_type)
+        #im = Image(source="dot.png", size=(im1.size[0]/2., im1.size[1]/2.), center=im1.center)
+        #im1.add_widget(im)
 
--- halfboard.py
+++ halfboard.py
@@ -2,7 +2,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.properties import NumericProperty
 from chesspiece import ChessPiece
 from kivy.uix.widget import Widget
-from kivy.core.window import Window
+from kivy.animation import Animation
 
 
 class HalfBoard(GridLayout):
@@ -13,11 +13,50 @@ class HalfBoard(GridLayout):
     def get_widget_at(self, row, column):
         return self.widgets_by_row_and_column[(row, column)]
 
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
+    def add_starting_pieces(self):
+        if self.half == "top":
+            back_row = 0
+            pawn_row = 3
+        else:
+            back_row = self.rows-1
+            pawn_row = self.rows-4
+
         for row in range(self.rows):
             for col in range(self.cols):
-                c = ChessPiece()#source="redpiece.png")
+                c = None
+                if row == back_row:
+                    # Set back row pieces
+                    if col == 0 or col == (self.cols-1):
+                        c = ChessPiece(piece_type="rook", player=self.player_color)
+                    elif col == 1 or col == (self.cols-2):
+                        c = ChessPiece(piece_type="knight", player=self.player_color)
+                    elif col == 2 or col == (self.cols - 3):
+                        c = ChessPiece(piece_type="elephant", player=self.player_color)
+                    elif col == 3 or col == (self.cols-4):
+                        c = ChessPiece(piece_type="guard", player=self.player_color)
+                    elif col == 4:
+                        c = ChessPiece(piece_type="king", player=self.player_color)
+                elif row == 2:
+                    # Add cannons
+                    if col == 1 or col == (self.cols-2):
+                        c = ChessPiece(piece_type="cannon", player=self.player_color)
+                elif row == pawn_row:
+                    # Add pawns
+                    if col in [0, 2, 4, 6, 8]:
+                        c = ChessPiece(piece_type="pawn", player=self.player_color)
+                if not c:
+                    c = Widget()
+                c.opacity = 0
                 self.add_widget(c)
                 self.widgets_by_row_and_column[(row, col)] = c
+
+        # Animate all the pieces into view
+        anim = Animation(opacity=1)
+        for child in self.walk():
+            if isinstance(child, ChessPiece):
+                anim.start(child)
+
+
+
+
 
