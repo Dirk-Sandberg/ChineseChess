b35472e1c7a39362ade2f7e9422525b53f798bf5
erik sandberg <eriksandbergum@gmail.com>
2020-05-01 18:09:53 -0500

    Added startup script for VM instance templates running the server

---
+++ communications/server_startup_script.sh
@@ -0,0 +1,9 @@
+# Install everything necessary to run the server
+# Could get this external IP and set it on firebase
+# or just put this instance in a group for a load balancer
+
+sudo apt install -y git
+git clone https://github.com/Dirk-Sandberg/ChineseChess.git
+sudo apt-get install -y python3-pip
+sudo pip3 install -r ChineseChess/communications/requirements.txt
+python3 ChineseChess/communications/server.py USERNAME PASSWORD
